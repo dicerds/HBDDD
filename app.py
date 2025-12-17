d1fcf8052334c5996a07dd201bc1594e2ab95212
Willy Susanto <136172743+dicerds@users.noreply.github.com>
2025-12-17 18:14:28 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def birthday():
+    data = {
+        "nama": "Natasha",
+        "age": 19,
+        "message": "[~] Selamat ulang tahunn [~]"
+    }
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
